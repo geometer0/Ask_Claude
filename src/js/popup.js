91d4e2a409029fd0d7d309e4686fbc0c8ebc1cc7
geometer0 <me@paulchristianson.io>
2025-02-13 13:02:27 -0500

    Popup now clears old text, displays loading message

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const responseDiv = document.getElementById('response');
+document.addEventListener("DOMContentLoaded", () => {
+    const responseDiv = document.getElementById("response");
     render(responseDiv);
     chrome.storage.onChanged.addListener(() => render(responseDiv));
-});    
+});
 
-async function render(div){
-    const result = await chrome.storage.local.get('popupText');
+async function render(div) {
+    const result = await chrome.storage.local.get("popupText");
     div.innerHTML = result.popupText;
-};
-window.addEventListener('blur', () => {
+}
+
+window.addEventListener("blur", async () => {
+    await chrome.storage.local.set({
+        popupText: "Getting response from Anthropic API...",
+    });
     window.close();
-  });
+});
